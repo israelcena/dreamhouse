041d87794ab570302b5a2d6f7d69d7f47fd90443
israelcena <israelsjm@gmail.com>
2022-07-20 14:00:25 -0300

    include home for rent table

---
+++ database/migrations/2022_07_20_165056_create_home_for_rents_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_for_rents', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo')->nullable();
+            $table->string('address')->nullable();
+            $table->string('condition');
+            $table->integer('value');
+            $table->integer('area');
+            $table->integer('bed');
+            $table->integer('bath');
+            $table->integer('parking');
+            $table->string('cep');
+            $table->string('state');
+            $table->fullText('description');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_for_rents');
+    }
+};
