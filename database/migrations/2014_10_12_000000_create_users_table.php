e2a44b4f8bc14d4c84e992677ff770f24d73679c
israelcena <israelsjm@gmail.com>
2022-07-20 13:41:53 -0300

    include users table fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf');
+            $table->string('phone');
+            $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
