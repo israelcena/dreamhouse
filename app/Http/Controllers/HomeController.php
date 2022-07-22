66f7c1a00aa24be0ba998deb9fbe0ea1eb6e4c3d
israelcena <israelsjm@gmail.com>
2022-07-21 23:27:21 -0300

    update show home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HomeForRent;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
+    public function __construct(
+        public HomeForRent $homeForRent,
+        public User $users
+    )
+    {
+    }
+
     public function index(): View
     {
-        return view('home');
+        $homes = $this->homeForRent::all()->random(4);
+        return view('home', compact('homes'));
     }
 }
