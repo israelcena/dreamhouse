32bda7a55a947dab4f7b8f2ce214ca24387e12a7
israelcena <israelsjm@gmail.com>
2022-07-27 22:26:01 -0300

    include search controler

--- app/Http/Controllers/HomeForRentController.php
+++ app/Http/Controllers/HomeForRentController.php
@@ -3,20 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\HomeForRent;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class HomeForRentController extends Controller
 {
     public function __construct(
-        public HomeForRent $homeForRent
+        public HomeForRent $homeForRent,
+        public User        $users,
+        public Request     $req
     )
     {
     }
 
-    public function index(Request $request): View
+    public function index(): View
     {
-        $search = $request->search;
-        return view('homeForRent.index', [compact('search')]);
+        $homes = $this->homeForRent->paginate(7);
+        $search = $this->req->search;
+        return view('pages.homeForRent.index', compact('homes', 'search'));
+    }
+
+    public function search(): RedirectResponse|View
+    {
+        $homes = $this->homeForRent->search($this->req->search)->get();
+        $search = $this->req->search;
+        if ($homes->count() === 0) {
+            return redirect()->route('homesForRent.index')->with('notfound', 'Desculpe, Sua busca não encontrou nenhum resultado !');
+        }
+        return view('pages.homeForRent.search', compact('homes', 'search'));
     }
 }
