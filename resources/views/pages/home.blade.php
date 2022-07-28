5387a8eabe5a3736cbb50c9b1ac090267f5894b0
israelcena <israelsjm@gmail.com>
2022-07-27 22:26:58 -0300

    change search route

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
             <div class="w-full md:w-2/3 flex flex-col mb-20 items-center text-center bg-white p-6 rounded-lg">
                 <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Vamos Encontrar Sua Casa dos Sonhos</h1>
                 <p class="mb-4 leading-relaxed">As melhores casas, os melhores corretores com a melhor plataforma.</p>
-                <form class="w-full" action="{{ route('homesForRent.index') }}" method="GET">
+                <form class="w-full" action="{{ route('homesForRent.search') }}" method="GET">
                 <div class="flex w-full justify-center items-end">
                     <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
                         <label for="search" class="leading-7 text-sm text-gray-600">Cidade ou endereço:</label>
@@ -83,40 +83,4 @@
             </div>
         </div>
     </section>
-
-{{--    Find Your Agent        --}}
-{{--    <section class="text-gray-600 body-font">--}}
-{{--        <div class="container px-5 py-24 mx-auto">--}}
-{{--            <div class="flex flex-wrap -m-4">--}}
-{{--                <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">--}}
-{{--                    <div class="h-full text-center">--}}
-{{--                        <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302">--}}
-{{--                        <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>--}}
-{{--                        <span class="inline-block h-1 w-10 rounded bg-yellow-500 mt-6 mb-4"></span>--}}
-{{--                        <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>--}}
-{{--                        <p class="text-gray-500">Senior Product Designer</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">--}}
-{{--                    <div class="h-full text-center">--}}
-{{--                        <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/300x300">--}}
-{{--                        <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>--}}
-{{--                        <span class="inline-block h-1 w-10 rounded bg-yellow-500 mt-6 mb-4"></span>--}}
-{{--                        <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">ALPER KAMU</h2>--}}
-{{--                        <p class="text-gray-500">UI Develeoper</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="lg:w-1/3 lg:mb-0 p-4">--}}
-{{--                    <div class="h-full text-center">--}}
-{{--                        <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/305x305">--}}
-{{--                        <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>--}}
-{{--                        <span class="inline-block h-1 w-10 rounded bg-yellow-500 mt-6 mb-4"></span>--}}
-{{--                        <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HENRY LETHAM</h2>--}}
-{{--                        <p class="text-gray-500">CTO</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </section>--}}
-
 @endsection
