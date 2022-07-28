88c93c862973f28359ff732f68a5ecf6adca441c
israelcena <israelsjm@gmail.com>
2022-07-27 23:08:07 -0300

    include show one link

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,34 +1,50 @@
 @extends('layouts.layout')
 @section('body')
     <section class="text-gray-600 body-font">
-        <div class="mx-full flex flex-col mb-12 px-1 pt-24 justify-center items-center bg-cover bg-no-repeat bg-center  bg-[url('https://www.homes.com/images/home/hero-desktop.jpg')]" >\
+        <div
+            class="mx-full flex flex-col mb-12 px-1 pt-24 justify-center items-center bg-cover bg-no-repeat bg-center  bg-[url('https://www.homes.com/images/home/hero-desktop.jpg')]">
+            \
             <div class="w-full md:w-2/3 flex flex-col mb-20 items-center text-center bg-white p-6 rounded-lg">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Vamos Encontrar Sua Casa dos Sonhos</h1>
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Vamos Encontrar Sua Casa dos
+                    Sonhos</h1>
                 <p class="mb-4 leading-relaxed">As melhores casas, os melhores corretores com a melhor plataforma.</p>
                 <form class="w-full" action="{{ route('homesForRent.search') }}" method="GET">
-                <div class="flex w-full justify-center items-end">
-                    <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
-                        <label for="search" class="leading-7 text-sm text-gray-600">Cidade ou endereço:</label>
-                        <input type="search" id="search" name="search" class="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-yellow-200 focus:bg-transparent border border-gray-300 focus:border-yellow-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" control-id="ControlID-82" placeholder="Ex: Rio de janeiro, Rua Almirante Barroso"/>
+                    <div class="flex w-full justify-center items-end">
+                        <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
+                            <label for="search" class="leading-7 text-sm text-gray-600">Cidade ou endereço:</label>
+                            <input type="search" id="search" name="search"
+                                   class="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-yellow-200 focus:bg-transparent border border-gray-300 focus:border-yellow-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                   control-id="ControlID-82"
+                                   placeholder="Ex: RJ, Rua Almirante Barroso, Apartamento..."/>
+                        </div>
+                        <button
+                            class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
+                            Buscar
+                        </button>
                     </div>
-                    <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Buscar</button>
-                </div>
                 </form>
                 <p class="text-sm mt-8 text-gray-500 mb-2 w-full">Baixe o nosso App</p>
                 <div class="flex">
-                    <button class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6" viewBox="0 0 512 512">
-                            <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
+                    <button
+                        class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6"
+                             viewBox="0 0 512 512">
+                            <path
+                                d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
                         </svg>
                         <span class="ml-4 flex items-start flex-col leading-none">
             <span class="text-xs text-gray-600 mb-1">GET IT ON</span>
             <span class="title-font font-medium">Google Play</span>
           </span>
                     </button>
-                    <button class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center ml-4 hover:bg-gray-200 focus:outline-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6" viewBox="0 0 305 305">
-                            <path d="M40.74 112.12c-25.79 44.74-9.4 112.65 19.12 153.82C74.09 286.52 88.5 305 108.24 305c.37 0 .74 0 1.13-.02 9.27-.37 15.97-3.23 22.45-5.99 7.27-3.1 14.8-6.3 26.6-6.3 11.22 0 18.39 3.1 25.31 6.1 6.83 2.95 13.87 6 24.26 5.81 22.23-.41 35.88-20.35 47.92-37.94a168.18 168.18 0 0021-43l.09-.28a2.5 2.5 0 00-1.33-3.06l-.18-.08c-3.92-1.6-38.26-16.84-38.62-58.36-.34-33.74 25.76-51.6 31-54.84l.24-.15a2.5 2.5 0 00.7-3.51c-18-26.37-45.62-30.34-56.73-30.82a50.04 50.04 0 00-4.95-.24c-13.06 0-25.56 4.93-35.61 8.9-6.94 2.73-12.93 5.09-17.06 5.09-4.64 0-10.67-2.4-17.65-5.16-9.33-3.7-19.9-7.9-31.1-7.9l-.79.01c-26.03.38-50.62 15.27-64.18 38.86z"></path>
-                            <path d="M212.1 0c-15.76.64-34.67 10.35-45.97 23.58-9.6 11.13-19 29.68-16.52 48.38a2.5 2.5 0 002.29 2.17c1.06.08 2.15.12 3.23.12 15.41 0 32.04-8.52 43.4-22.25 11.94-14.5 17.99-33.1 16.16-49.77A2.52 2.52 0 00212.1 0z"></path>
+                    <button
+                        class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center ml-4 hover:bg-gray-200 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6"
+                             viewBox="0 0 305 305">
+                            <path
+                                d="M40.74 112.12c-25.79 44.74-9.4 112.65 19.12 153.82C74.09 286.52 88.5 305 108.24 305c.37 0 .74 0 1.13-.02 9.27-.37 15.97-3.23 22.45-5.99 7.27-3.1 14.8-6.3 26.6-6.3 11.22 0 18.39 3.1 25.31 6.1 6.83 2.95 13.87 6 24.26 5.81 22.23-.41 35.88-20.35 47.92-37.94a168.18 168.18 0 0021-43l.09-.28a2.5 2.5 0 00-1.33-3.06l-.18-.08c-3.92-1.6-38.26-16.84-38.62-58.36-.34-33.74 25.76-51.6 31-54.84l.24-.15a2.5 2.5 0 00.7-3.51c-18-26.37-45.62-30.34-56.73-30.82a50.04 50.04 0 00-4.95-.24c-13.06 0-25.56 4.93-35.61 8.9-6.94 2.73-12.93 5.09-17.06 5.09-4.64 0-10.67-2.4-17.65-5.16-9.33-3.7-19.9-7.9-31.1-7.9l-.79.01c-26.03.38-50.62 15.27-64.18 38.86z"></path>
+                            <path
+                                d="M212.1 0c-15.76.64-34.67 10.35-45.97 23.58-9.6 11.13-19 29.68-16.52 48.38a2.5 2.5 0 002.29 2.17c1.06.08 2.15.12 3.23.12 15.41 0 32.04-8.52 43.4-22.25 11.94-14.5 17.99-33.1 16.16-49.77A2.52 2.52 0 00212.1 0z"></path>
                         </svg>
                         <span class="ml-4 flex items-start flex-col leading-none">
             <span class="text-xs text-gray-600 mb-1">Download on the</span>
@@ -42,19 +58,29 @@
 
     <section class="text-gray-600 body-font">
         <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
-            <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Conectamos Corretores
                     <br class="hidden lg:inline-block">Compradores e Vendedores.
                 </h1>
-                <p class="mb-8 leading-relaxed">DreamHouse está construindo um mercado colaborativo de última geração para conectar compradores, vendedores e agentes e equipá-los com as ferramentas certas em cada etapa do processo. Com mais de um milhão de listagens e recursos inovadores, vamos ajudá-lo a tomar a decisão mais informada, seja comprando sua casa dos sonhos ou vendendo um imóvel. Estamos redefinindo o processo de compra de casas.</p>
+                <p class="mb-8 leading-relaxed">DreamHouse está construindo um mercado colaborativo de última geração
+                    para conectar compradores, vendedores e agentes e equipá-los com as ferramentas certas em cada etapa
+                    do processo. Com mais de um milhão de listagens e recursos inovadores, vamos ajudá-lo a tomar a
+                    decisão mais informada, seja comprando sua casa dos sonhos ou vendendo um imóvel. Estamos
+                    redefinindo o processo de compra de casas.</p>
                 <div class="flex justify-center">
-                    <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
+                    <button
+                        class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
                         <a href="{{ route('homesForRent.index') }}">Explorar</a></button>
-                    <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Cadastre-se</button>
+                    <button
+                        class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                        Cadastre-se
+                    </button>
                 </div>
             </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img class="object-cover object-center rounded" alt="hero" src="https://www.homes.com/images/home/seamlessly-connect.jpg">
+                <img class="object-cover object-center rounded" alt="hero"
+                     src="https://www.homes.com/images/home/seamlessly-connect.jpg">
             </div>
         </div>
     </section>
@@ -66,22 +92,28 @@
                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">As Casas Mais Amadas</h1>
                     <div class="h-1 w-20 bg-yellow-500 rounded"></div>
                 </div>
-                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Uma seleção com as melhores casas! Aproveite e veja mais!</p>
+                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Uma seleção com as melhores casas! Aproveite e
+                    veja mais!</p>
             </div>
             <div class="flex flex-wrap -m-4">
                 @if($homes)
-                @foreach($homes as $home)
-                <div class="xl:w-1/4 md:w-1/2 p-4">
-                    <div class="bg-gray-100 p-6 rounded-lg h-100">
-                        <img class="h-40 rounded w-full object-cover object-center mb-6" src="{{ $home->photo }}" alt="content">
-                        <h3 class="tracking-widest text-yellow-500 text-xs font-medium title-font">{{ $home->condition }}</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">R$ {{ $home->value }},00</h2>
-                        <p class="leading-relaxed text-base">{{ "$home->type com $home->bed Quartos, $home->bath Banheiros e com $home->area m² de area total." }} </p>
-                    </div>
-                </div>
-                @endforeach
+                    @foreach($homes as $home)
+                        <div class="xl:w-1/4 md:w-1/2 p-4">
+                            <a href="{{ route('homesForRent.show', $home->id) }}">
+                                <div class="bg-gray-100 p-6 rounded-lg h-100 hover:bg-gray-200 ease-in duration-200">
+                                    <img class="h-40 rounded w-full object-cover object-center mb-6"
+                                         src="{{ $home->photo }}" alt="content">
+                                    <h3 class="tracking-widest text-yellow-500 text-xs font-medium title-font">{{ $home->condition }}</h3>
+                                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">R$ {{ $home->value }}
+                                        ,00</h2>
+                                    <p class="leading-relaxed text-base">{{ "$home->type com $home->bed Quartos, $home->bath Banheiros e com $home->area m² de area total." }} </p>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
                     <div class="flex justify-center w-full mt-4">
-                        <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded-lg text-lg">
+                        <button
+                            class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded-lg text-lg">
                             <a href="{{ route('homesForRent.index') }}">Explorar Todas</a>
                         </button>
                     </div>
