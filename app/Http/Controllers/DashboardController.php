394404cb7d8222c245ecc08ee72d50f570e0a426
israelcena <israelsjm@gmail.com>
2022-08-04 14:49:03 -0300

    include show all homes

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeForRent;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+
+    public function __construct(
+        public User $user,
+        public HomeForRent $homeForRent
+    ) {
+    }
+
+    public function index(Auth $auth): View
+    {
+        $InUser = $auth::user();
+        $InUser->load('homeForRent');
+        $homesOfUser = $InUser->homeForRent;
+        return view('dashboard', compact('InUser', 'homesOfUser'));
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <p>Parabéns! Você está logado!</p>
+                    <p>{{ $InUser->name }}, Você está logado!</p>
+                <br/>
+                    <p>Suas Informações: </p>
+                    <p>Cpf: {{ $InUser->cpf }}</p>
+                    <p>Telefone: {{ $InUser->phone }}</p>
+                    <p>Endereço: {{ $InUser->address }}</p>
                 </div>
             </div>
         </div>
@@ -21,66 +26,31 @@
                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Casas Cadastradas</h1>
             </div>
             <div class="flex flex-wrap -m-4">
+                @foreach ($homesOfUser as $home)
                 <div class="lg:w-1/3 sm:w-1/2 p-4">
                     <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/600x360">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={{ $home->photo }} >
                         <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">{{ $home->type }} {{ $home->condition }}</h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">R$ {{ $home->value }}</h1>
+                            <p class="leading-relaxed">{{ $home->type }} {{ $home->condition }} com {{ $home->bed }} quartos, {{ $home->bath }} banheiros, {{ $home->parking }} vagas de garagem e com {{ $home->area }} metros quadrados de espaço.</p>
+                            <div class="mt-4 flex gap-4">
+                            <button
+                            class="text-white bg-yellow-500 border-0 py-1 px-4 focus:outline-none hover:bg-yellow-500 rounded hover:bg-yellow-400 focus:outline-none">
+                            Editar
+                            </button>
+                            <form action="">
+                            <button
+                            class="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-500 rounded hover:bg-red-400 focus:outline-none">
+                            Excluir
+                            </button>
+                            </form>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
-                <div class="lg:w-1/3 sm:w-1/2 p-4">
-                    <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/601x361">
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lg:w-1/3 sm:w-1/2 p-4">
-                    <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/603x363">
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lg:w-1/3 sm:w-1/2 p-4">
-                    <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/602x362">
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lg:w-1/3 sm:w-1/2 p-4">
-                    <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/605x365">
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lg:w-1/3 sm:w-1/2 p-4">
-                    <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/606x366">
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">THE SUBTITLE</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
