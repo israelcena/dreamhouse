d0959aae51b32c277199486a6f78b28d4f3be29a
israelcena <israelsjm@gmail.com>
2022-08-03 23:55:07 -0300

    include New menu item

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('')">
+                        {{ __('Cadastrar Nova Casa') }}
+                    </x-nav-link>
                 </div>
             </div>
 
