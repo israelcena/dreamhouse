371b7c0119fa7fd4048bd8dc24ef171b286d50a1
Israel Cena <israelsjm@gmail.com>
2022-08-09 20:27:44 -0300

    include greetings text

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -31,7 +31,7 @@
                 </a>
                 <div class="lg:w-2/5 inline-flex items-center lg:justify-end ml-5 lg:ml-0 gap-2">
                     @if (Auth::check())
-                        <a href="{{ route('dashboard.index') }}">{{ Auth::user()->name }}</a>
+                        <a class="text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out" href="{{ route('dashboard.index') }}">Olá! {{ Auth::user()->name }}</a>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
